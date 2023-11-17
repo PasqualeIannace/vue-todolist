60bdbbaabdbd28f440ebae84e0b9266a2565e90d
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-17 18:00:36 +0100

    MILESTONE 2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
+createApp({
+	data() {
+		return {
+			todo: [
+				{
+					text: "Studiare",
+					done: false
+				},
+				{
+					text: "Fare la spesa",
+					done: false
+				},
+				{
+					text: "Lavare l'auto",
+					done: false
+				},
+				{
+					text: "Allenamento",
+					done: false
+				}
+			]
+
+			
+		}
+	},
+	methods: {
+		changeStatus(i) {
+			console.log("click!", i);
+			
+			if(this.todo[i].done){
+				this.todo[i].done = false;
+			} else {
+				this.todo[i].done = true;
+			}
+		}
+	},
+	mounted() {
+        console.log(`Pagina caricata`);
+        console.log(this.todo);
+    },
+}).mount('#app')
+
